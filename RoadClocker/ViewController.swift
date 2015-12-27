1266871409fa04e44dc203ab5192d9694d791d2b
Zvi <zvi@skeevisarts.com>
2015-12-27 11:08:32 -0500

    Initial Commit

---
+++ RoadClocker/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RoadClocker
+//
+//  Created by Zvi Band on 12/27/15.
+//  Copyright © 2015 skeevisarts. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
