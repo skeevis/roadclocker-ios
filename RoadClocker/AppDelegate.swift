5efdedf4c1c41dc6c2b28cd980d7c8da86a75bdf
Zvi <zvi@skeevisarts.com>
2016-01-23 08:47:48 -0500

    finally got all the background alerting working...

--- RoadClocker/AppDelegate.swift
+++ RoadClocker/AppDelegate.swift
@@ -7,16 +7,40 @@
 //
 
 import UIKit
+import CoreLocation
+
+var driveDatabase = [[String:Double]]()
+var beaconInRange = false
+var beaconInRange2 = false
+var beaconStartTimestamp = NSDate.timeIntervalSinceReferenceDate()
+
+var timer = NSTimer()
+var elapsedTime: NSTimeInterval = -1
+
 
 @UIApplicationMain
-class AppDelegate: UIResponder, UIApplicationDelegate {
+class AppDelegate: UIResponder, UIApplicationDelegate, CLLocationManagerDelegate {
 
     var window: UIWindow?
 
+    
+    var locationManager = CLLocationManager()
+    
+    
+    //I was hoping that you could just find all beacons, and choose which one you not. Nope.
+    //So this is hardcoded, and any beacons we use in the future will have to have this UUID.
+    let BEACON_UUID = "10CAE1B7-3F02-4936-BDBB-2D3F93A02EE0"
+    let BEACON_MAJOR = 1
+    let BEACON_MINOR = 1
+    
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         let settings = UIUserNotificationSettings(forTypes: [.Alert, .Badge, .Sound], categories: nil)
         application.registerUserNotificationSettings(settings)
+        locationManager = CLLocationManager()
+        locationManager.delegate = self;
+        locationManager.requestAlwaysAuthorization()
+        beaconSearch()
         return true;
     }
 
@@ -41,6 +65,127 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationWillTerminate(application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
+    
+    
+    
+    func beaconSearch(){
+        if (CLLocationManager.authorizationStatus() != CLAuthorizationStatus.AuthorizedAlways) {
+            locationManager.requestAlwaysAuthorization()
+        }
+        locationManager.delegate = self
+        locationManager.pausesLocationUpdatesAutomatically = false
+        let beaconRegion = CLBeaconRegion(proximityUUID: NSUUID(UUIDString:BEACON_UUID)!, identifier: "primaryBeacon")
+        beaconRegion.notifyEntryStateOnDisplay = true
+        beaconRegion.notifyOnEntry = true
+        beaconRegion.notifyOnExit = true
+        locationManager.startMonitoringForRegion(beaconRegion)
+        locationManager.startUpdatingLocation()
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        print("Fired up. Searching")
+    }
+    
+    
+    func locationManager(manager: CLLocationManager, didEnterRegion region: CLRegion) {
+        print("IN RANGE")
+        let notification = UILocalNotification()
+        notification.alertBody = "Drive Started. We'll keep track of the time!"
+        UIApplication.sharedApplication().scheduleLocalNotification(notification)
+        setBackgroundColor("Immediate")
+        beaconInRange = true
+        beaconStartTimestamp = NSDate.timeIntervalSinceReferenceDate()
+        timer = NSTimer.scheduledTimerWithTimeInterval(1, target: self, selector: "updateTime", userInfo: nil, repeats: true)
+
+    }
+    
+    func locationManager(manager: CLLocationManager, didExitRegion region: CLRegion) {
+        print("NOT IN RANGE")
+        setBackgroundColor("Gone")
+        if(beaconInRange){
+            beaconInRange = false
+            timer.invalidate()
+            let startTime =  NSDate().timeIntervalSince1970
+            let timeElapsed = NSDate.timeIntervalSinceReferenceDate() - beaconStartTimestamp
+            driveDatabase.insert(["startTime":startTime, "timeElapsed":timeElapsed], atIndex: 0)
+            let notification = UILocalNotification()
+            notification.alertBody = "Drive Completed. Total time \(prettyPrintElapsedTime(timeElapsed))"
+            UIApplication.sharedApplication().scheduleLocalNotification(notification)
+            mainWindow().savePreviousDrives()
+        }
+        mainWindow().tableView.reloadData()
+    }
+    
+    
+    func nameForProximity(proximity: CLProximity) -> String {
+        switch proximity {
+        case .Unknown:
+            return "Unknown"
+        case .Immediate:
+            return "Immediate"
+        case .Near:
+            return "Near"
+        case .Far:
+            return "Far"
+        }
+    }
+    
+    func mainWindow() -> ViewController{
+        let viewController:ViewController = window!.rootViewController as! ViewController
+        return viewController
+    }
+    
+    func updateTime() {
+        let currentTime = NSDate.timeIntervalSinceReferenceDate()
+        elapsedTime = currentTime - beaconStartTimestamp
+        let viewController:ViewController = window!.rootViewController as! ViewController
+        viewController.tableView!.reloadData()
+    }
+    
+    
+    func prettyPrintElapsedTime(elapsedTimeToDisplay: NSTimeInterval) ->String
+    {
+        var elapsedTimeToDisplay2 = elapsedTimeToDisplay
+        let hours = UInt8(elapsedTimeToDisplay / 3600.0)
+        elapsedTimeToDisplay2 -= (NSTimeInterval(hours) * 3600)
+        let minutes = UInt8(elapsedTimeToDisplay2 / 60.0)
+        elapsedTimeToDisplay2 -= (NSTimeInterval(minutes) * 60)
+        let seconds = UInt8(elapsedTimeToDisplay2)
+        elapsedTimeToDisplay2 -= NSTimeInterval(seconds)
+        let strHours = String(format: "%02d", hours)
+        let strMinutes = String(format: "%02d", minutes)
+        let strSeconds = String(format: "%02d", seconds)
+        return ("\(strHours):\(strMinutes):\(strSeconds)")
+        
+    }
+    
+    
+    func setBackgroundColor(location: String){
+        print("COLOR")
+        
+        let viewController:ViewController = window!.rootViewController as! ViewController
+        let view = viewController.view
+
+        
+        switch location{
+        case "Immediate":
+            print("IMMEDIATE")
+            view.backgroundColor = UIColor(red: 41/255, green: 128/255, blue: 185/255, alpha: 1.0)
+        case "Near":
+            print("NEAR")
+            view.backgroundColor = UIColor(red: 41/255, green: 128/255, blue: 185/255, alpha: 1.0)
+        case "Far":
+            print("FAR")
+            view.backgroundColor = UIColor(red: 41/255, green: 128/255, blue: 185/255, alpha: 1.0)
+        case "Gone":
+            print("GONE")
+            view.backgroundColor = UIColor(red: 231/255, green: 76/255, blue: 60/255, alpha: 1.0)
+        default:
+            print("DEFAULT")
+            view.backgroundColor = UIColor(red: 231/255, green: 76/255, blue: 60/255, alpha: 1.0)
+        }
+        
+    }
+    
+
     
 
 
--- RoadClocker/ViewController.swift
+++ RoadClocker/ViewController.swift
@@ -10,32 +10,12 @@ import UIKit
 import CoreLocation
 
 
-class ViewController: UIViewController,CLLocationManagerDelegate, UITableViewDataSource, UITableViewDelegate {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
-    let locationManager = CLLocationManager()
-
-    
-    //I was hoping that you could just find all beacons, and choose which one you not. Nope.
-    //So this is hardcoded, and any beacons we use in the future will have to have this UUID.
-    let BEACON_UUID = "10CAE1B7-3F02-4936-BDBB-2D3F93A02EE0"
-    let BEACON_MAJOR = 1
-    let BEACON_MINOR = 1
-    
-    var beaconInRange = false
-    var beaconStartTimestamp = NSDate.timeIntervalSinceReferenceDate()
-
-    var timer = NSTimer()
-    var elapsedTime: NSTimeInterval = -1
-    
-    var driveDatabase = [[String:Double]]()
-    
     @IBOutlet weak var tableView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        locationManager.delegate = self;
-        locationManager.requestAlwaysAuthorization()
-        beaconSearch()
         getPreviousDrives()
         tableView.reloadData()
         tableView.rowHeight = 60
@@ -47,95 +27,6 @@ class ViewController: UIViewController,CLLocationManagerDelegate, UITableViewDat
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
-    
-    
-
-
-    func beaconSearch(){
-        if (CLLocationManager.authorizationStatus() != CLAuthorizationStatus.AuthorizedAlways) {
-            locationManager.requestAlwaysAuthorization()
-        }
-        let beaconRegion = CLBeaconRegion(proximityUUID: NSUUID(UUIDString:BEACON_UUID)!, identifier: "primaryBeacon")
-        locationManager.startRangingBeaconsInRegion(beaconRegion)
-        print("Fired up. Searching")
-    }
-    
-    func locationManager(manager: CLLocationManager, didRangeBeacons beacons: [CLBeacon], inRegion region: CLBeaconRegion) {
-        let knownBeacons = beacons.filter{ $0.proximity != CLProximity.Unknown }
-        print(knownBeacons)
-        if(knownBeacons.count > 0 && !beaconInRange)
-        {
-            print("Beacon is now \(nameForProximity(knownBeacons.first!.proximity))")
-            beaconInRange = true
-            beaconStartTimestamp = NSDate.timeIntervalSinceReferenceDate()
-            timer = NSTimer.scheduledTimerWithTimeInterval(1, target: self, selector: "updateTime", userInfo: nil, repeats: true)
-            setBackgroundColor(nameForProximity(knownBeacons.first!.proximity))
-        }
-        else if(knownBeacons.count > 0)
-        {
-            print("Beacon is now \(nameForProximity(knownBeacons.first!.proximity))")
-            beaconInRange = true
-            setBackgroundColor(nameForProximity(knownBeacons.first!.proximity))
-        }
-        else if(beaconInRange){
-            print("No beacon in range anymore")
-            beaconInRange = false
-            timer.invalidate()
-            let startTime =  NSDate().timeIntervalSince1970
-            let timeElapsed = NSDate.timeIntervalSinceReferenceDate() - beaconStartTimestamp
-            driveDatabase.insert(["startTime":startTime, "timeElapsed":timeElapsed], atIndex: 0)
-            savePreviousDrives()
-            print(driveDatabase)
-            
-            setBackgroundColor("Gone")
-            let notification = UILocalNotification()
-            notification.alertBody = "Drive Over."
-            UIApplication.sharedApplication().scheduleLocalNotification(notification)
-            tableView.reloadData()
-        }
-    }
-    
-   
-    func nameForProximity(proximity: CLProximity) -> String {
-        switch proximity {
-        case .Unknown:
-            return "Unknown"
-        case .Immediate:
-            return "Immediate"
-        case .Near:
-            return "Near"
-        case .Far:
-            return "Far"
-        }
-    }
-    func updateTime() {
-        
-        let currentTime = NSDate.timeIntervalSinceReferenceDate()
-
-        
-        elapsedTime = currentTime - beaconStartTimestamp
-        
-        tableView.reloadData()
-        
-        //bunch of fucking failures here. I want to blow my brains out with this shit.
-        
-//        if(self.tableView.indexPathsForVisibleRows?.count > 0){
-//            tableView.reloadRowsAtIndexPaths([self.tableView.indexPathsForVisibleRows!.last!], withRowAnimation: UITableViewRowAnimation.None)
-//        }
-//
-//let indexPath = NSIndexPath(forRow: 0, inSection: 0)
-//        let indices = self.tableView.indexPathsForVisibleRows
-//        print(indices)
-//        if(indices?.first?.row == 0)
-//        {
-//
-//            
-//        }
-//        if(beaconInRange && tableView.numberOfSections > 0 && tableView.numberOfRowsInSection(1) > 0){
-//            tableView.reloadRowsAtIndexPaths([indexPath], withRowAnimation: UITableViewRowAnimation.None)
-//        }
-    }
-    
     
     func prettyPrintElapsedTime(elapsedTimeToDisplay: NSTimeInterval) ->String
     {
@@ -150,9 +41,7 @@ class ViewController: UIViewController,CLLocationManagerDelegate, UITableViewDat
         let strMinutes = String(format: "%02d", minutes)
         let strSeconds = String(format: "%02d", seconds)
         return ("\(strHours):\(strMinutes):\(strSeconds)")
-
     }
-    
     
     func setBackgroundColor(location: String){
         switch location{
@@ -167,12 +56,7 @@ class ViewController: UIViewController,CLLocationManagerDelegate, UITableViewDat
             default:
                 self.view.backgroundColor = UIColor(red: 231/255, green: 76/255, blue: 60/255, alpha: 1.0)
         }
-
     }
-    
-    
-
-    
     
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int{
         return (beaconInRange ? 1 : 0) + driveDatabase.count
@@ -251,9 +135,12 @@ class ViewController: UIViewController,CLLocationManagerDelegate, UITableViewDat
           driveDatabase = [[String:Double]]()
           defaults.setValue(driveDatabase, forKey: "driveDatabase")
         }
+        print("Previous drives")
+        print(driveDatabase)
         //for debugging locally
         //driveDatabase = [["timeElapsed": 16.9998490214348, "startTime": 1451241623.74958], ["timeElapsed": 12.9996680021286, "startTime": 1451241589.31375], ["timeElapsed": 9.99890094995499, "startTime": 1451241572.31298], ["timeElapsed": 6.99492001533508, "startTime": 1451241557.31374]]
         defaults.synchronize()
+        tableView.reloadData()
     }
     
     func savePreviousDrives()
@@ -262,8 +149,8 @@ class ViewController: UIViewController,CLLocationManagerDelegate, UITableViewDat
         defaults.setValue(driveDatabase, forKey: "driveDatabase")
         defaults.synchronize()
     }
+
     
     
     
 }
-
